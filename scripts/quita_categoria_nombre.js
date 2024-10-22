fb7ec1cef86ed220595874c5ac42b9d38e3286a8
Luciano <lucho.2012.tandil@gmail.com>
2024-10-22 19:44:34 -0300

    Se actualiza script para quitar categoria de nombre de producto

--- scripts/quita_categoria_nombre.js
+++ scripts/quita_categoria_nombre.js
@@ -1,4 +1,7 @@
 require("dotenv").config({ path: '../.env' })
+const fs = require('fs');
+
+const NOMBRE_ARCHIVO = "updates_prod_quita_cat.sql"
 
 let conn_obj = {
     host: process.env.mysql_host,
@@ -27,15 +30,40 @@ const knex = require('knex')({
 
 setTimeout(async () => { 
     let productos       = await knex('products')
-    let categorias      = await knex('product_category')
+    let categorias      = await knex('category')
     
     if (productos && categorias){
-        for (let i=0; i < productos.length; i++){
-            let nuevo_name = productos[i].name.split(" - ").slice(1).join(" - ")
-            
-            await knex('products').where({id: productos[i].id}).update({name: nuevo_name})
-            await knex('alias_productos').where({product_id: productos[i].id}).update({alias: nuevo_name})
-            console.log(nuevo_name)
+        let diccio_cats = {}
+        for (let i=0; i < categorias.length; i++){
+            diccio_cats[categorias[i].name.trim().toLowerCase()] = categorias[i]
         }
+        //console.log(diccio_cats)
+        //return
+        let sql = ""
+
+        for (let i=0; i < productos.length; i++){
+            let split_ = productos[i].name.split(" - ")
+            let text_cat = String(split_[0]).trim().toLowerCase()
+            if (!diccio_cats[split_[0]]){
+                console.log("no tiene categoria en nombre", "|",text_cat,"|")
+                continue
+            }
+
+            let nuevo_name = split_.slice(1).join(" - ")
+            if (nuevo_name == '') continue
+            console.log('nuevo_name',nuevo_name)
+
+            sql += `UPDATE products SET name = '${nuevo_name}' WHERE id = "${productos[i].id}";\n`
+            sql += `UPDATE alias_productos SET alias = '${nuevo_name}' WHERE product_id = "${productos[i].id}";\n`           
+        }
+
+        fs.writeFileSync(NOMBRE_ARCHIVO, sql, (err) => {
+            if (err) {
+                console.error(`Error al escribir el archivo: ${err.message}`)
+            } else {
+                console.log(`Archivo escrito correctamente:`, NOMBRE_ARCHIVO)
+                
+            }
+        })
     }
 }, 1000)
