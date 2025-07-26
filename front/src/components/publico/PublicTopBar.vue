4e0211b5c41b1de79792730f0934b309be3def7e
luciano <lucho.2012.tandil@gmail.com>
2025-07-26 07:40:51 -0300

    chore(front): comentar temporalmente el enlace de menú a Estadísticas en el topbar público

--- front/src/components/publico/PublicTopBar.vue
+++ front/src/components/publico/PublicTopBar.vue
@@ -95,7 +95,7 @@ const enlaces = ref([
     //{ path: '/categorias', title: 'Navegar por Categorías' },
     { path: '/ofertas', title: 'Ofertas' },
     { path: '/carga_precio', title: 'Carga de Precios' },
-    { path: '/estadisticas', title: 'Estadísticas' },
+    // { path: '/estadisticas', title: 'Estadísticas' }, // <--- ENLACE COMENTADO TEMPORALMENTE
     { path: '/calcula_trueque', title: 'Calcula Trueque' },
     { path: '/aporta', url: "https://cafecito.app/tandil_precios", title: 'Quiero Aportar' }
 ])
