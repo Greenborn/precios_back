2bba056eb70774afc9f710f52aa0b574cb9774fa
luciano <lucho.2012.tandil@gmail.com>
2025-07-26 07:50:39 -0300

    fix: corregir fecha en promociones_hoy para usar new Date() en lugar de argentinaToUTC

--- back/routes/productos.js
+++ back/routes/productos.js
@@ -261,7 +261,7 @@ async function procesar_oferta(trx, item, HOY, AYER){
             
                 const insert_ = {
                     'orden': 0,
-                    'fecha': argentinaToUTC(HOY),
+                    'fecha': new Date(),
                     'titulo': item.titulo,
                     'id_producto': -1,
                     'precio': item.precio,
